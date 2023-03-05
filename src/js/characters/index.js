104cd76946e66da850c83f290defd11ff879731e
Родионов Алексей Игоревич <rodionov_a@tii.ru>
2023-03-05 20:15:35 +0300

    del

--- src/js/characters/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Bowman } from "./Bowman";
-export { default as Daemon } from "./Daemon";
-export { default as Magician } from "./Magician";
-export { default as Undead } from "./Undead";
-export { default as Vampire } from "./Vampire";
-export { default as Swordsman } from "./Swordsman";
